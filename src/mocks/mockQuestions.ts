f7e036216795472d97f9283d0e363a0172a96f1a
Dzmitry Kisel <dimakisel333@gmail.com>
2024-04-17 19:49:07 +0300

    Add mock data for questions

---
+++ src/mocks/mockQuestions.ts
@@ -0,0 +1,86 @@
+export const mockQuestions = [
+  {
+    id: 1,
+    type: 'single_choice',
+    question: 'Что означает аббревиатура HTML?',
+    options: ['Hyper Text Markup Language', 'Hyperlinks and Text Markup Language', 'Home Tool Markup Language', 'Hyper Tool Multi Language']
+  },
+  {
+    id: 2,
+    type: 'single_choice',
+    question: 'Какое расширение имеют файлы стилей CSS?',
+    options: ['.css', '.html', '.js', '.php']
+  },
+  {
+    id: 3,
+    type: 'multiple_choice',
+    question: 'Какие языки программирования используются для создания интерактивности на веб-страницах?',
+    options: ['JavaScript', 'PHP', 'Python', 'Ruby']
+  },
+  {
+    id: 4,
+    type: 'short_answer',
+    question: 'Как называется основная структурная единица веб-страницы?',
+  },
+  {
+    id: 5,
+    type: 'long_answer',
+    question: 'Опишите принцип работы протокола HTTP.',
+  },
+  {
+    id: 6,
+    type: 'single_choice',
+    question: 'Как объявить переменную в JavaScript?',
+    options: ['let', 'var', 'const', 'all of the above']
+  },
+  {
+    id: 7,
+    type: 'single_choice',
+    question: 'Что выведет console.log(2 + 3 + "7")?',
+    options: ['57', '12', '23', 'NaN']
+  },
+  {
+    id: 8,
+    type: 'multiple_choice',
+    question: 'Какие встроенные методы есть у массивов в JavaScript?',
+    options: ['push()', 'has()', 'keys()', 'values()', 'entries()', 'clear()', 'pop()', 'size()']
+  },
+  {
+    id: 9,
+    type: 'long_answer',
+    question: 'Что такое замыкание (closure) в JavaScript?',
+  },
+  {
+    id: 10,
+    type: 'long_answer',
+    question: 'Расскажите о принципе работы промисов (promises) в JavaScript.',
+  },
+  {
+    id: 11,
+    type: 'single_choice',
+    question: 'Как создать новый React-проект с использованием Create React App?',
+    options: ['npx create-react-app my-app', 'npm init react-app my-app', 'create-react my-app', 'npm create-react my-app']
+  },
+  {
+    id: 12,
+    type: 'single_choice',
+    question: 'Что такое JSX в React?',
+    options: ['Расширение JavaScript, которое позволяет писать HTML-подобный код в JavaScript', 'JavaScript библиотека для работы с DOM', 'Шаблонизатор для React-компонентов', 'Препроцессор для стилей в React-приложениях']
+  },
+  {
+    id: 13,
+    type: 'multiple_choice',
+    question: 'Какие методы жизненного цикла существуют у компонентов React?',
+    options: ['componentDidMount', 'componentWillUnmount', 'componentWillUpdate', 'shouldComponentUpdate', 'render']
+  },
+  {
+    id: 14,
+    type: 'long_answer',
+    question: 'Что такое state в React?',
+  },
+  {
+    id: 15,
+    type: 'long_answer',
+    question: 'Чем отличается компонент React от функционального компонента и классового компонента?',
+  },
+];
